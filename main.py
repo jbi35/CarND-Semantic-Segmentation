86dfde600572955864bc7a2241a63fef205d1cce
jbi <biehler@adco-engineering-gw.com>
2017-09-02 20:26:57 +0200

    completed load vgg function to load pre-saved vgg network

--- main.py
+++ main.py
@@ -24,16 +24,28 @@ def load_vgg(sess, vgg_path):
     :param vgg_path: Path to vgg folder, containing "variables/" and "saved_model.pb"
     :return: Tuple of Tensors from VGG model (image_input, keep_prob, layer3_out, layer4_out, layer7_out)
     """
-    # TODO: Implement function
-    #   Use tf.saved_model.loader.load to load the model and weights
+    # load the vgg model and weights
     vgg_tag = 'vgg16'
+    tf.saved_model.loader.load(sess, [vgg_tag], vgg_path)
+
     vgg_input_tensor_name = 'image_input:0'
     vgg_keep_prob_tensor_name = 'keep_prob:0'
     vgg_layer3_out_tensor_name = 'layer3_out:0'
     vgg_layer4_out_tensor_name = 'layer4_out:0'
     vgg_layer7_out_tensor_name = 'layer7_out:0'
-    
-    return None, None, None, None, None
+
+    # get graph
+    default_graph = tf.get_default_graph()
+
+    # get layers from graph
+    vgg_input = default_graph.get_tensor_by_name(vgg_input_tensor_name)
+    vgg_keep_prob  = default_graph.get_tensor_by_name(vgg_keep_prob_tensor_name)
+    vgg_layer3_out = default_graph.get_tensor_by_name(vgg_layer3_out_tensor_name)
+    vgg_layer4_out = default_graph.get_tensor_by_name(vgg_layer4_out_tensor_name)
+    vgg_layer7_out = default_graph.get_tensor_by_name(vgg_layer7_out_tensor_name)
+
+    return vgg_input, vgg_keep_prob, vgg_layer3_out, vgg_layer4_out, vgg_layer7_out
+
 tests.test_load_vgg(load_vgg, tf)
 
 
